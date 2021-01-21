67dcd5a2744005f1bfbad8b011a5849fa76c1e7c
LurkePoint <49284978+LurkePoint@users.noreply.github.com>
2021-01-22 00:15:39 +0300

    Add files via upload

---
+++ js/main-slider.js
@@ -0,0 +1,15 @@
+$(function () {
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        autoplay: true,
+        autoplayHoverPause: false,
+        loop: true,
+        autoplayTimeout: 5000,
+        smartSpeed: 1500,
+        // arrows: false,
+        // dots: false,
+        // autoplay: true, 
+        // autoplaySpeed: 5500,
+        // speed: 1500
+    })
+})
